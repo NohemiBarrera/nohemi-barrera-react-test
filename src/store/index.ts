d700e1ed37f1905c9b94e414df536b6d23d5d90f
NohemiBarrera <nohemi.baol@gmail.com>
2024-11-12 14:23:37 -0600

    Add missing types, simplify store configuration

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,31 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, AnyAction } from '@reduxjs/toolkit'
 import authReducer from './slices/authSlice'
 import productsReducer from './slices/productsSlice'
 import { logout } from './slices/authSlice'
+import { AuthState, ProductsState } from '../types'
+
+interface RootReducer {
+  auth: AuthState
+  products: ProductsState
+}
 
 const rootReducer = {
   auth: authReducer,
   products: productsReducer,
-}
+} as const
+
+const createInitialState = (): RootReducer => ({
+  auth: authReducer(undefined, { type: '' }),
+  products: productsReducer(undefined, { type: '' }),
+})
 
 export const store = configureStore({
-  reducer: (state, action) => {
-    // Clear all state when logging out
+  reducer: (state: RootReducer | undefined, action: AnyAction): RootReducer => {
     if (action.type === logout.type) {
-      // Clear localStorage
       localStorage.clear()
-
-      // Return initial state for all reducers
-      return Object.keys(rootReducer).reduce((acc, key) => {
-        acc[key] = rootReducer[key](undefined, action)
-        return acc
-      }, {})
+      return createInitialState()
     }
 
-    return Object.keys(rootReducer).reduce((acc, key) => {
-      acc[key] = rootReducer[key](state?.[key], action)
-      return acc
-    }, {})
+    const currentState = state ?? createInitialState()
+    return {
+      auth: authReducer(currentState.auth, action),
+      products: productsReducer(currentState.products, action),
+    }
   },
 })
 
